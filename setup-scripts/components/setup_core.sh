d8f1d4f20836a4c87bc908bc5ae0cf930626ce23
Matteo Barbieri <neopard@gmail.com>
2021-07-16 15:19:39 +0200

    Add bat and fd to core installation
    `bat` and `fd` are super useful alternatives to the common `cat` and
    `find` commands respectively.
    
    More information on what they are at their github pages
    - https://github.com/sharkdp/bat
    - https://github.com/sharkdp/fd

--- setup-scripts/components/setup_core.sh
+++ setup-scripts/components/setup_core.sh
@@ -37,4 +37,22 @@ setup_core()
   # Create symlink to .aliases file
   ln -s `realpath "${SCRIPTPATH}/../dotfiles/.aliases"` ~/.aliases
 
+  # Download and install bat (cool replacement for cat)
+  # TODO make it system-dependent (should download a different package depending
+  # on the machine on which it is installed)
+  wget \
+    https://github.com/sharkdp/bat/releases/download/v0.18.2/bat_0.18.2_amd64.deb \
+    -O ~/Downloads/bat_0.18.2_amd64.deb
+
+  sudo dpkg -i ~/Downloads/bat_0.18.2_amd64.deb
+
+  # Download and install `fd` (user friendlier replacement of `find`)
+  # TODO make it system-dependent (should download a different package depending
+  # on the machine on which it is installed)
+  wget \
+    https://github.com/sharkdp/fd/releases/download/v8.2.1/fd_8.2.1_amd64.deb \
+    -O ~/Downloads/fd_8.2.1_amd64.deb
+
+  sudo dpkg -i ~/Downloads/fd_8.2.1_amd64.deb
+
 }
