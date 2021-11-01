e96014b835fbab568847b32aa097c39308f84849
Matteo Barbieri <matteo.a.barbieri@gmail.com>
2021-11-01 12:01:08 +0000

    Fix the installation script for the core components

--- setup-scripts/components/setup_core.sh
+++ setup-scripts/components/setup_core.sh
@@ -2,6 +2,8 @@
 
 # Core packages such as git, tmux and openssh
 
+[ $(whoami) = root ] || SUDO=sudo
+
 setup_core()
 {
 
@@ -11,7 +13,9 @@ setup_core()
 
   # Install git, tmux, openssh-server,
   # cmake, c
-  sudo apt install -y \
+  $SUDO apt update
+
+  $SUDO apt install -y \
   git \
   tmux \
   htop \
@@ -23,28 +27,30 @@ setup_core()
   silversearcher-ag
 
   # Add custom git aliases to .gitconfig
-  cat `realpath "${SCRIPTPATH}/../dotfiles/.gitconfig"` >> ~/.gitconfig
+  cat `realpath "${SCRIPTPATH}/dotfiles/.gitconfig"` >> ~/.gitconfig
 
   # Install tmux plugin manager
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
   # Create symlink to .tmux.conf
-  ln -s `realpath "${SCRIPTPATH}/../dotfiles/.tmux.conf"` ~/.tmux.conf
+  ln -s `realpath "${SCRIPTPATH}/dotfiles/.tmux.conf"` ~/.tmux.conf
 
   # Create backup copy of existing ~/.aliases file
   [ -f ~/.aliases ] && mv ~/.aliases ~/.aliases.bak
 
   # Create symlink to .aliases file
-  ln -s `realpath "${SCRIPTPATH}/../dotfiles/.aliases"` ~/.aliases
+  ln -s `realpath "${SCRIPTPATH}/dotfiles/.aliases"` ~/.aliases
 
   # Download and install bat (cool replacement for cat)
   # TODO make it system-dependent (should download a different package depending
   # on the machine on which it is installed)
+  mkdir -p ~/Downloads
+
   wget \
     https://github.com/sharkdp/bat/releases/download/v0.18.2/bat_0.18.2_amd64.deb \
     -O ~/Downloads/bat_0.18.2_amd64.deb
 
-  sudo dpkg -i ~/Downloads/bat_0.18.2_amd64.deb
+  $SUDO dpkg -i ~/Downloads/bat_0.18.2_amd64.deb
 
   # Download and install `fd` (user friendlier replacement of `find`)
   # TODO make it system-dependent (should download a different package depending
@@ -53,6 +59,6 @@ setup_core()
     https://github.com/sharkdp/fd/releases/download/v8.2.1/fd_8.2.1_amd64.deb \
     -O ~/Downloads/fd_8.2.1_amd64.deb
 
-  sudo dpkg -i ~/Downloads/fd_8.2.1_amd64.deb
+  $SUDO dpkg -i ~/Downloads/fd_8.2.1_amd64.deb
 
 }
