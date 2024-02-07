59268333906b1448aed87798f6c28cea41803a23
Matteo Barbieri <neopard@gmail.com>
2024-02-07 10:22:33 +0100

    Add installation scripts for docker and minikube in k8s component

--- setup-scripts/components/setup_k8s.sh
+++ setup-scripts/components/setup_k8s.sh
@@ -15,7 +15,23 @@ setup_k8s()
   $SUDO ls > /dev/null
 
   # Docker
-  # TODO
+  # Add Docker's official GPG key:
+  $SUDO apt-get update
+  $SUDO apt-get install ca-certificates curl
+  $SUDO install -m 0755 -d /etc/apt/keyrings
+  $SUDO curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  $SUDO chmod a+r /etc/apt/keyrings/docker.asc
+  # Install docker
+  $SUDO apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  # Add user to docker group
+  $SUDO usermod -aG docker $(whoami)
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    $SUDO tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $SUDO apt-get update
 
   # Terraform
   wget -O- https://apt.releases.hashicorp.com/gpg | $SUDO gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
@@ -23,7 +39,8 @@ setup_k8s()
   $SUDO apt update && $SUDO apt install terraform
 
   # Minikube
-  # TODO
+  curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+  $SUDO dpkg -i minikube_latest_amd64.deb
 
   # Helm
   # TODO
